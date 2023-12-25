b840618867eb01911ad10f582763e692407ac14b
luis <luisfelipemourapereira@protonmail.com>
2023-12-25 18:17:36 +0100

    init a bin

--- provision_hennersz_nixos_2305.sh
+++ provision_hennersz_nixos_2305.sh
@@ -13,3 +13,34 @@ else
     mkdir -p $DIR && git clone -b $BRANCH $REPO
   fi
 fi
+
+RBENV_DIR=~/.rbenv
+RBENV_REPO=https://github.com/rbenv/rbenv.git
+RBENV_BRANCH=master
+RUBYBUILD_DIR=$RBENV_DIR/plugins/ruby-build
+RUBYBUILD_REPO=https://github.com/rbenv/ruby-build.git
+RUBYBUILD_BRANCH=master
+GEMSET_DIR=$RBENV_DIR/plugins/rbenv-gemset
+GEMSET_REPO=https://github.com/jf/rbenv-gemset.git
+GEMSET_BRANCH=master
+
+if ! [ -d $RBENV_DIR ]; then
+  git clone \
+    -b $RBENV_BRANCH \
+    $RBENV_REPO \
+    $RBENV_DIR
+fi
+
+if ! [ -d $RUBYBUILD_DIR ]; then
+  git clone \
+    -b $RUBYBUILD_BRANCH \
+    $RUBYBUILD_REPO \
+    $RUBYBUILD_DIR
+fi
+
+if ! [ -d $GEMSET_DIR ]; then
+  git clone \
+    -b $GEMSET_BRANCH \
+    $GEMSET_REPO \
+    $GEMSET_DIR
+fi
