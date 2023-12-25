a49d0295301c10da8f864af7a60dfff401a1c854
luis <luisfelipemourapereira@protonmail.com>
2023-12-25 18:35:45 +0100

    init a bin

--- provision_hennersz_nixos_2305.sh
+++ provision_hennersz_nixos_2305.sh
@@ -10,37 +10,11 @@ if [ "$LOCAL" -eq 1 ]; then
   nix-env -iA nixos.nfs-utils
 else
   if ! [ -d $DIR ]; then
-    mkdir -p $DIR && git clone -b $BRANCH $REPO
+    mkdir -p $DIR && git clone -b $BRANCH $REPO $DIR
   fi
 fi
 
-RBENV_DIR=~/.rbenv
-RBENV_REPO=https://github.com/rbenv/rbenv.git
-RBENV_BRANCH=master
-RUBYBUILD_DIR=$RBENV_DIR/plugins/ruby-build
-RUBYBUILD_REPO=https://github.com/rbenv/ruby-build.git
-RUBYBUILD_BRANCH=master
-GEMSET_DIR=$RBENV_DIR/plugins/rbenv-gemset
-GEMSET_REPO=https://github.com/jf/rbenv-gemset.git
-GEMSET_BRANCH=master
+cd $DIR || return 
 
-if ! [ -d $RBENV_DIR ]; then
-  git clone \
-    -b $RBENV_BRANCH \
-    $RBENV_REPO \
-    $RBENV_DIR
-fi
-
-if ! [ -d $RUBYBUILD_DIR ]; then
-  git clone \
-    -b $RUBYBUILD_BRANCH \
-    $RUBYBUILD_REPO \
-    $RUBYBUILD_DIR
-fi
-
-if ! [ -d $GEMSET_DIR ]; then
-  git clone \
-    -b $GEMSET_BRANCH \
-    $GEMSET_REPO \
-    $GEMSET_DIR
-fi
+nix flake update
+nix flake show
