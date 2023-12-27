417233a1d6c2f0706ddfd253af8b2a73b71d90aa
luis <luisfelipemourapereira@protonmail.com>
2023-12-27 07:00:53 -0500

    init home-manager

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
       url = github:NixOS/nixpkgs?branch=release-23.11;
     };
     home-manager = {
-      url = github:drzln/home-manager?branch=release-23.11;
+      url = github:nix-community/home-manager?branch=release-23.11;
       # home-manager's nixpkgs dependency is the same as ours
       inputs.nixpkgs.follows = "nixpkgs";
     };
