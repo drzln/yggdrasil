c077bef2cee9d4fd03ff021abdcf59e208853881
luis <luisfelipemourapereira@protonmail.com>
2023-12-27 06:43:22 -0500

    init home-manager

--- flake.nix
+++ flake.nix
@@ -12,45 +12,56 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
-    nixosConfigurations = {
-      yggdrasil = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { };
-        modules = [
-          home-manager.nixosModules.home-manager
-          ({ pkgs, ... }: {
-            system.stateVersion = "24.05";
-            boot.loader.grub = {
-              enable = true;
-              version = 2;
-              device = "nodev";
-            };
-            fileSystems."/" = {
-              device = "/dev/vda1";
-              fsType = "ext4";
-            };
-            networking.hostName = "yggdrasil";
-            networking.useDHCP = false;
-            networking.interfaces.eth0.useDHCP = true;
-            users.users.luis = {
-              isNormalUser = true;
-              home = "/home/luis";
-              description = "luis";
-              packages = [ pkgs.home-manager ];
-            };
-            time.timeZone = "UTC";
-            services.openssh.enable = true;
-            home-manager.users.luis = {
-              programs.home-manager.enable = true;
-              home.stateVersion = "23.05";
-              home.sessionVariables = {
-                EDITOR = "vim";
-              };
-            };
-          })
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      fcitx = self: super: {
+        fcitx-engines = super.fcitx5;
+      };
+      pkgs = import nixpkgs {
+        overlays = [
+          fcitx
         ];
       };
+    in
+    {
+      nixosConfigurations = {
+        yggdrasil = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit pkgs; };
+          modules = [
+            home-manager.nixosModules.home-manager
+            ({ pkgs, ... }: {
+              system.stateVersion = "23.11";
+              boot.loader.grub = {
+                enable = true;
+                version = 2;
+                device = "nodev";
+              };
+              fileSystems."/" = {
+                device = "/dev/vda1";
+                fsType = "ext4";
+              };
+              networking.hostName = "yggdrasil";
+              networking.useDHCP = false;
+              networking.interfaces.eth0.useDHCP = true;
+              users.users.luis = {
+                isNormalUser = true;
+                home = "/home/luis";
+                description = "luis";
+                packages = [ pkgs.home-manager ];
+              };
+              time.timeZone = "UTC";
+              services.openssh.enable = true;
+              home-manager.users.luis = {
+                programs.home-manager.enable = true;
+                home.stateVersion = "23.11";
+                home.sessionVariables = {
+                  EDITOR = "vim";
+                };
+              };
+            })
+          ];
+        };
+      };
     };
-  };
 }
