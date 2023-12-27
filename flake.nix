5bbb7966500aca1620348dcc5f70b551c40a64b9
luis <luisfelipemourapereira@protonmail.com>
2023-12-27 04:58:31 -0500

    init home-manager

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager }: {
     nixosConfigurations = {
       yggdrasil = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
